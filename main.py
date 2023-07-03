3d49a8c7a2291afa8328bd1c79e02f51ee42e0d6
Wei Xiang Ong <xiangweiong@gmail.com>
2023-07-03 23:46:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Load the webpage
 # timezone = 27 = GMT+8
-url = 'https://ec.forexprostools.com/?columns=exc_currency,exc_importance&importance=1,2,3&calType=day&timeZone=27&lang=1'
+# url = 'https://ec.forexprostools.com/?columns=exc_currency,exc_importance&importance=1,2,3&calType=day&timeZone=27&lang=1'
+url = 'https://ec.forexprostools.com/?calType=day&timeZone=27&lang=1'
 driver.get(url)
 
 # Wait for the table to load (adjust the wait time as needed)
@@ -46,13 +47,16 @@ for event_row in event_rows:
         currency = cells[1].text.strip()
         sentiment = cells[2].get_attribute('title')
         event = cells[3].text.strip()
+        actual = cells[4].text.strip()
+        forecast = cells[5].text.strip()
+        previous = cells[6].text.strip()
 
         if "High Volatility Expected" in sentiment:
-            high_events += f"Time: {time} | Currency: {currency} | Event: {event}\n"
+            high_events += f"Time: {time} | Currency: {currency} | Event: {event}\n | Forecast: {forecast}\n | Previous: {previous}\n"
          
-        all_events += f"Time: {time}\nCurrency: {currency}\nImportance: {sentiment}\nEvent: {event}\n"
+        all_events += f"Time: {time}\nCurrency: {currency}\nImportance: {sentiment}\nEvent: {event}\nActual: {actual}\nForecast: {forecast}\nPrevious: {previous}\n\n"
 
-message = f"Daily Forex News Alert - High Impact (SGT)\n\n[{high_events}](https://ec.forexprostools.com/?columns=exc_currency,exc_importance&importance=1,2,3&calType=day&timeZone=27&lang=1)"
+message = f"Daily Forex News Alert - High Impact - SGT\n\n{high_events}\n\n[All news]({url})"
 
 requests.get(
     f"https://api.telegram.org/{bot}/sendMessage?chat_id={chat_id}&text="
