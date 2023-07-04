6c484194fc5c3227675331462c38718095271923
Wei Xiang Ong <xiangweiong@gmail.com>
2023-07-04 15:22:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import requests
 import os
+from urllib.parse import quote
 
 # Initialize
 bot = os.environ['TELEGRAM_BOT_TOKEN']
@@ -55,12 +56,13 @@ for event_row in event_rows:
          
         all_events += f"Time: {time}\nCurrency: {currency}\nImportance: {sentiment}\nEvent: {event}\nActual: {actual}\nForecast: {forecast}\nPrevious: {previous}\n\n"
 
-message = f"Daily Forex News Alert - High Impact - SGT\n\n{high_events}[all news for today]({url})"
+encoded_url = quote(url, safe='')
+message = f"Daily Forex News Alert - High Impact - SGT\n\n{high_events}[all news for today]({encoded_url})"
+
 
 requests.get(
     f"https://api.telegram.org/{bot}/sendMessage?chat_id={chat_id}&text="
-    #+ f"{message}"
-    + message
+    + f"{message}"
     + "&parse_mode=markdown&disable_web_page_preview=True"
 )
 
