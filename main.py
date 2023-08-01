0466ceab994d4a389e00dbc6e2ff1e90bf9f985a
Wei Xiang Ong <xiangweiong@gmail.com>
2023-08-01 14:41:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ def scrape_forex_events():
     service = Service(chromedriver_path)
 
     # Start the WebDriver
-    # driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe"))
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     # Load the webpage
     # timezone = 27 = GMT+8
