7663d3a12f2f238a376a6d7641cecb3804d0384c
Wei Xiang Ong <xiangweiong@gmail.com>
2023-08-01 14:07:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
