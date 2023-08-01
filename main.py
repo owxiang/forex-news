6ad32b1ed1887b0a297e475b9694066ab745800a
Wei Xiang Ong <xiangweiong@gmail.com>
2023-08-01 16:53:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,20 +17,6 @@ def scrape_forex_events():
     # Create a string variable to hold the table
     table_for_all_md = table_header_for_all
     table_for_low_md = table_for_moderate_md = table_for_high_md = table_header_for_the_rest
-    
-    # # Path to the ChromeDriver executable
-    # # chromedriver_path = '/path/to/chromedriver'
-    # chromedriver_path = '/usr/local/bin'
-
-    # # Set Chrome options to run in headless mode
-    # chrome_options = Options()
-    # chrome_options.add_argument('--headless')  # Run Chrome in headless mode
-
-    # # Start the ChromeDriver service
-    # service = Service(chromedriver_path)
-
-    # # Start the WebDriver
-    # driver = webdriver.Chrome(service=service, options=chrome_options)
 
     # Set Chrome options to run in headless mode
     chrome_options = Options()
