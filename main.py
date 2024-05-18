1e50b8d47729a817aa5bdb6063c6b1a2e4f728d5
Wei Xiang Ong <ongweixiang@icloud.com>
2024-05-18 12:22:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ def scrape_forex_events():
 
     # Run Chrome in headless mode
     chrome_options.add_argument('--headless')  
-    
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+
     # Start the WebDriver
     driver = webdriver.Chrome(options=chrome_options)
 
