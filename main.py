0ef0f0cde1cbfc225ffa6277caf45d08ef44371b
Wei Xiang Ong <ongweixiang@icloud.com>
2024-07-03 23:21:22 +0800

    fix: import timeoutexception

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from datetime import datetime
 import requests
